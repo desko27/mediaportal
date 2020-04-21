92d3992b55c67c9b23298d1ddf2c9fa1d35f9750
desko27 <desko27@gmail.com>
2020-04-21 09:53:34 +0200

    Improve FileList/Item styles

--- src/components/FileList/FileItem/index.js
+++ src/components/FileList/FileItem/index.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import cx from 'classnames'
 
+import { ReactComponent as RadioIcon } from './icons/radio.svg'
+import { ReactComponent as FilmIcon } from './icons/film.svg'
+import { ReactComponent as ImageIcon } from './icons/image.svg'
+
 import styles from './index.module.css'
 
 const FileItem = ({ file, isSelected, onFileClick }) => {
-  const { name, path } = file
+  const { name, path, type } = file
   return (
     <div className={cx(styles.wrapper, isSelected && styles.isSelected)}>
       <div className={styles.fileState}>
-        {isSelected && <span role='img' aria-label='pin'>📍</span>}
+        {isSelected && <RadioIcon />}
       </div>
       <button
         key={path}
         className={styles.fileButton}
         onClick={() => onFileClick(file)}
       >
-        {name}
+        <span className={styles.fileButtonType}>
+          {type === 'video' ? <FilmIcon /> : <ImageIcon />}
+        </span>
+        <span className={styles.fileButtonText}>
+          {name}
+        </span>
       </button>
     </div>
   )
