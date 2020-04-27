1da148cb70cd39c5a07d2025a0091760bf5f4a77
desko27 <desko27@gmail.com>
2020-04-27 18:02:32 +0200

    Add reordering items feature by dragging

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import cx from 'classnames'
 import { useDropzone } from 'react-dropzone'
+import { Droppable } from 'react-beautiful-dnd'
 
 import FileItem from './FileItem'
 import styles from './index.module.css'
@@ -10,9 +11,9 @@ const FileList = ({
   className,
   currentFile,
   fileList,
+  onDropFiles,
   onFileClick,
   onStateClick,
-  onDropFiles,
   willRemoveChecks
 }) => {
   const { getRootProps, isDragActive } = useDropzone({ onDrop: onDropFiles })
@@ -20,23 +21,33 @@ const FileList = ({
   return (
     <div {...getRootProps({ className: cx(styles.wrapper, className, isDragActive && styles.draggingPlaceholder) })}>
       {!isDragActive && (
-        <div className={styles.fileStack}>
-          {fileList.map(file => {
-            const isSelected = currentFile && currentFile.id === file.id
-            const isChecked = checkedFiles.includes(file.id)
-            return (
-              <FileItem
-                key={file.id}
-                file={file}
-                isSelected={isSelected}
-                onFileClick={onFileClick}
-                onStateClick={onStateClick}
-                isChecked={isChecked}
-                willRemoveChecks={willRemoveChecks}
-              />
-            )
-          })}
-        </div>
+        <Droppable droppableId='file-list'>
+          {provided => (
+            <div
+              className={styles.fileStack}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {fileList.map((file, index) => {
+                const isSelected = currentFile && currentFile.id === file.id
+                const isChecked = checkedFiles.includes(file.id)
+                return (
+                  <FileItem
+                    key={file.id}
+                    index={index}
+                    file={file}
+                    isSelected={isSelected}
+                    onFileClick={onFileClick}
+                    onStateClick={onStateClick}
+                    isChecked={isChecked}
+                    willRemoveChecks={willRemoveChecks}
+                  />
+                )
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       )}
     </div>
   )
