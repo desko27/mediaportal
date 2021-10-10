6b95e795408002a1802aafef5788171099474ed3
desko27 <desko27@gmail.com>
2021-10-10 20:12:14 +0200

    Replace react-scripts by vite

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
+
+export default defineConfig({
+  plugins: [react(), svgrPlugin()]
+})
