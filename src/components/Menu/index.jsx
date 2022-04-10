51430af9bf1f3d62191dc2ae363e0d959580247c
desko27 <desko27@gmail.com>
2022-04-10 15:49:20 +0200

    Replace classnames package with lighter clsx

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import cx from 'classnames'
+import cx from 'clsx'
 import { FormattedMessage, useIntl } from 'react-intl'
 import { version } from '../../../package.json'
 
